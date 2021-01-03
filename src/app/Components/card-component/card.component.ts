077a5b5ac6e2ff8975033a556c25ebe1c7da5dc3
p3choco <1piotrbednarski1@gmail.com>
2021-01-03 20:57:25 +0100

    Basic card display done

--- src/app/Components/card-component/card.component.ts
+++ src/app/Components/card-component/card.component.ts
@@ -17,8 +17,8 @@ import { TaskDto } from 'src/app/Logic/Dtos/task.dto';
     ngOnInit(): void {
         this.tasks = [
             {
-                name: "First",
-                deadline: new Date(),
+                name: "Rysunek na Instagram",
+                deadline: new Date("2021-01-30"),
                 isDone: false,
                 penaltyForDay: 10,
             },
@@ -28,10 +28,42 @@ import { TaskDto } from 'src/app/Logic/Dtos/task.dto';
                 isDone: false,
                 penaltyForDay: 1000,
             },
+            {
+                name: "Gra w pilke",
+                deadline: new Date(),
+                isDone: false,
+                penaltyForDay: 0,
+            },
+            {
+                name: "Programowanie",
+                deadline: new Date(),
+                isDone: false,
+                penaltyForDay: 1000,
+            },
+            {
+                name: "Kupić kwiaty dla mamy i taty",
+                deadline: new Date(),
+                isDone: false,
+                penaltyForDay: 10,
+            },
+            {
+                name: "Cos",
+                deadline: new Date(),
+                isDone: false,
+                penaltyForDay: 1000,
+            },
+            {
+                name: "Second",
+                deadline: new Date("2019-6-5"),
+                isDone: false,
+                penaltyForDay: 1000,
+            },
         ];
         console.log(this.tasks);
     }
-    
+    public calculateTimeToExecute(task: TaskDto): number {
+         return  Math.floor(( task.deadline.getTime()-new Date().getTime())/(1000 * 3600 * 24));
+    }
 
 
   }
